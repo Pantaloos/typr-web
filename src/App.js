25be40ef84b770c6db547ac7907e5f516fd8b60e
Vordty <tokomostwanted@gmail.com>
2022-05-14 16:14:20 +0400

    Add examples of Text and Button components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import Text from "components/Text/Text";
+import Button from "components/Button/Button";
+
 function App() {
-	return <div className="App">YO</div>;
+	return (
+		<div className="App">
+			<Text type="giant" customStyle="landing-title color-light">
+				Typr - Example Giant
+			</Text>
+			<Text type="header" customStyle="landing-title color-light">
+				Typr - Example Giant
+			</Text>
+			<Button type="action" customStyle="mt-4">
+				PLAY
+			</Button>
+		</div>
+	);
 }
 
 export default App;
