7fabbb7b95a5bffd5b42f949419b006fb5e8db1d
levan buxnikashvili <levanifenix.1234@gmail.com>
2022-06-04 20:07:11 +0400

    added button component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Text from "components/Text/Text";
 import Button from "components/Button/Button";
+import {ReactComponent as ReactLogo} from "./icons/rightArrow.svg"	
 
 function App() {
 	return (
@@ -10,6 +11,20 @@ function App() {
 			</Text>
 			<Button type="action" customStyle="mt-4">
 				PLAY
+			</Button>
+			<div></div>
+			<Button type="filled" customStyle="mt-4">
+				START/READY
+			</Button>
+			<div></div>
+			<Button type="outline" customStyle="mt-4">
+				basic
+			</Button>
+			<div/>
+			<Button type="filled-with-icon" customStyle="mt-4" style={{width: "50px", height : "50px"}}>
+				<div className="icon-container">
+				<ReactLogo width = {25} height = {25} />
+				</div>
 			</Button>
 		</div>
 	);
