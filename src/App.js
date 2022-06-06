aaf90c430525e2d8a3aaa09382f2d96809ed8c45
Irakli Kubaneishvili <irakli.kubaneishvili00@gmail.com>
2022-06-06 18:23:15 +0400

    321

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import Home from "routes/Home";
-import {BrowserRouter as Router,Routes, Route} from 'react-router-dom'
-function App() {
-	return (
-		<Router>
-			<Routes>
-				<Route path="/" element={<Home />}></Route>
-			</Routes>
-		</Router>
-	);
-}
+import Dropdown from "components/Dropdown/Dropdown";
+import React from "react";
+
+const App = () => {
+  const items = [
+    {
+      id: 1,
+      value: "60 Seconds",
+    },
+    {
+      id: 2,
+      value: "90 Seconds",
+    },
+    {
+      id: 3,
+      value: "120 Seconds",
+    },
+  ];
+
+  return (
+    <div className="container">
+      <Dropdown
+        initialTitle="Select Time"
+        items={items}
+        onValueChange={(value) => console.log(value)}
+      ></Dropdown>
+    </div>
+  );
+};
 
 export default App;
-
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,42 +3,72 @@ import Input from "components/Input/Input";
 import Room from "components/Room/Room";
 import Text from "components/Text/Text";
 import "./Home.scss";
-function home() {
-	return (
-		<div className="rootDiv">
-            <Text type="giant" customStyle="siteName"> TYPR.IO</Text>
-            <div id="top-container">
-                <div id="left-div">
-                    <Button type="filled" customStyle="left-btn" > PLAY</Button>
-                    <div className="top-container">
-                        <Text type="basic" customStyle="center-text">JOIN A ROOM</Text>
-                        <div style={{display:"flex"}}>
-                            
-                            <div className="inputs-container">
-                                <Input type="single" label="CODE" customLabelClass="label-class" customInputClass="input-code" maxLength="4" ></Input>
-                            </div>
+const home = () => {
+  return (
+    <div className="rootDiv">
+      <Text type="giant" customStyle="siteName">
+        {" "}
+        TYPR.IO
+      </Text>
+      <div id="top-container">
+        <div id="left-div">
+          <Button type="filled" customStyle="left-btn">
+            {" "}
+            PLAY
+          </Button>
+          <div className="top-container">
+            <Text type="basic" customStyle="center-text">
+              JOIN A ROOM
+            </Text>
+            <div style={{ display: "flex" }}>
+              <div className="inputs-container">
+                <Input
+                  type="single"
+                  label="CODE"
+                  customLabelClass="label-class"
+                  customInputClass="input-code"
+                  maxLength="4"
+                ></Input>
+              </div>
 
-                            <div className="inputs-container">
-                                <Input type="single" label="NICKNAME" customLabelClass="label-class" customInputClass="input-code" maxLength="12" ></Input>
-                            </div>
-
-                        </div>
-                        <Button type="filled"  customStyle="left-btn"> JOIN</Button>
-                    </div>
-                </div>
-
-                
-                <div id="right-div">
-                    <Text type="basic" customStyle="center-text">AVAILABLE ROOMS</Text>
-                    <div className="rootDiv background-dark-a fillWidth">
-                        <Room roomName="test" numberOfPeople="8" customStyle="mt-4 background-dark-b"></Room>
-                        <Room roomName="levante" numberOfPeople="4" customStyle="mt-4 background-dark-b"></Room>
-                    </div>
-                </div>
+              <div className="inputs-container">
+                <Input
+                  type="single"
+                  label="NICKNAME"
+                  customLabelClass="label-class"
+                  customInputClass="input-code"
+                  maxLength="12"
+                ></Input>
+              </div>
             </div>
-		</div>
-	);
-}
+            <Button type="filled" customStyle="left-btn">
+              {" "}
+              JOIN
+            </Button>
+          </div>
+        </div>
+
+        <div id="right-div">
+          <Text type="basic" customStyle="center-text">
+            AVAILABLE ROOMS
+          </Text>
+          <div className="rootDiv background-dark-a fillWidth">
+            <Room
+              roomName="test"
+              numberOfPeople="8"
+              customStyle="mt-4 background-dark-b"
+            ></Room>
+            <Room
+              roomName="levante"
+              numberOfPeople="4"
+              customStyle="mt-4 background-dark-b"
+            ></Room>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default home;
 
