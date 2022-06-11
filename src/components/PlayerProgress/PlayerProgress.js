5a18f145d81f8852f4ac77a39c9cf574e2ad6553
Levani Bukhnikashvili <levanifenix.1234@gmail.com>
2022-06-11 14:15:02 +0400

    Refactor PlayerProgress

--- src/components/PlayerProgress/PlayerProgress.js
+++ src/components/PlayerProgress/PlayerProgress.js
@@ -1,33 +1,25 @@
-import "./PlayerProgress.scss";
+import "./PlayerProgress.scss"
 
 const PlayerProgress = ({
-	type = "lobby",
-	textType = "basic",
-	customStyle = "",
-	textCustomStyle="",
-	name = "",
-	children,
-	iconWidth = 50,
-	iconHeight= 50,
-    progressProcentage = 0,
-	playerIcon = 'placeholder_player_image.png',
-	...props
+  containerCustomStyle = "",
+  imageCustomStyle = "",
+  textCustomStyle = "",
+  progressProcentage = 0,
+  playerIcon = "placeholder_player_image.png",
 }) => {
-	const iconTypeClass = type;
-	const textTypeClass = textType;
-	const iconClassName = `${iconTypeClass} ${textTypeClass}-text ${customStyle}`;
+  const images = require.context("../../assets/icons/", true)
+  const image = images(`./${playerIcon}`)
 
-	const images = require.context('../../assets/icons/', true);
-	const image = images(`./${playerIcon}`);
+  const imageClass = `${imageCustomStyle} progress-image-style`
+  const textClass = `${textCustomStyle} percentage-style`
+  const containerClass = `${containerCustomStyle} progress-container`
 
-	return (
-		<div className={iconClassName} style={{display:"flex", flexDirection:"column"}}>
-			<img src={image} alt="Avatar" style={{width:iconWidth , height: iconHeight , borderRadius: iconHeight/ 2}}></img>
-            <span className={textCustomStyle} style={{display:"table", margin:"0 auto" ,color: "#b4caea"}}>
-				{progressProcentage}%
-			</span>
-		</div>
-	);
-};
+  return (
+    <div className={containerClass}>
+      <img src={image} alt="Avatar" className={imageClass}></img>
+      <span className={textClass}>{progressProcentage}%</span>
+    </div>
+  )
+}
 
-export default PlayerProgress;
+export default PlayerProgress
