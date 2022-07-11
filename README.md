b68e08c88131750a850c193f6887540339cd8d27
Irakli Kubaneishvili <irakli.kubaneishvili00@gmail.com>
2022-07-11 23:35:45 +0400

    Add readme

---
+++ README.md
@@ -0,0 +1,15 @@
+## Prerequisites
+
+- Node.js 18+
+
+## Install necessary packages
+
+```properties
+npm install
+```
+
+## Launch the project
+
+```properties
+npm run start
+```
--- src/routes/Results/Results.js
+++ src/routes/Results/Results.js
@@ -6,81 +6,85 @@ import { useParams, useNavigate, useLocation } from "react-router-dom";
 import { SocketContext } from "../../socket";
 
 const Results = () => {
-	const { id: gameId } = useParams();
-	const socket = useContext(SocketContext);
-	const navigate = useNavigate();
-	const { state } = useLocation();
-	const { nickname, roomCode } = state;
+  const { id: gameId } = useParams();
+  const socket = useContext(SocketContext);
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const { nickname, roomCode } = state;
 
-	const [results, setResults] = useState([]);
+  const [results, setResults] = useState([]);
 
-	useEffect(() => {
-		fetch(`https://${process.env.REACT_APP_API_URL}/result/game/${gameId}`)
-			.then(response => {
-				return response.json();
-			})
-			.then(data => {
-				setResults(data);
-			});
+  useEffect(() => {
+    fetch(`http://${process.env.REACT_APP_API_URL}/result/game/${gameId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setResults(data);
+      });
 
-		socket.on("resultsUpdated", data => {
-			setResults(data.results);
-		});
-	}, []);
+    socket.on("resultsUpdated", (data) => {
+      setResults(data.results);
+    });
+  }, []);
 
-	return (
-		<div className="flex flex-v height100h">
-			<div customStyle="main-div">
-				<Box title="RESULTS" customStyle="header-padding" contentCustomStyle="container-height">
-					<div className="flex-h space-between">
-						<div className="flex-v">
-							{results.map(data => (
-								<span key={data.user.id} className="label-class label-text">
-									{data.user.name}
-								</span>
-							))}
-						</div>
-						<div className="flex-v">
-							{results.map(data => (
-								<span key={data.user.id} className="points-color">
-									{" "}
-									{data.points} points
-								</span>
-							))}
-						</div>
-						<div className="flex-v">
-							{results.map(data => (
-								<span key={data.user.id} className="label-class label-text">
-									{data.time} seconds / {data.mistakes} mistakes
-								</span>
-							))}
-						</div>
-					</div>
-				</Box>
-			</div>
-			<div>
-				<Button
-					title="filled-btn"
-					text="BACK"
-					customStyle="my-button-left"
-					onClick={() => {
-						socket.emit("leaveRoom");
-						navigate(`/`);
-					}}
-				></Button>
-				<Button
-					title="filled-btn"
-					text="PLAY AGAIN"
-					customStyle="my-button-right"
-					onClick={() => {
-						navigate(`/room/${roomCode}`, {
-							state: { nickname: nickname },
-						});
-					}}
-				></Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-v height100h">
+      <div customStyle="main-div">
+        <Box
+          title="RESULTS"
+          customStyle="header-padding"
+          contentCustomStyle="container-height"
+        >
+          <div className="flex-h space-between">
+            <div className="flex-v">
+              {results.map((data) => (
+                <span key={data.user.id} className="label-class label-text">
+                  {data.user.name}
+                </span>
+              ))}
+            </div>
+            <div className="flex-v">
+              {results.map((data) => (
+                <span key={data.user.id} className="points-color">
+                  {" "}
+                  {data.points} points
+                </span>
+              ))}
+            </div>
+            <div className="flex-v">
+              {results.map((data) => (
+                <span key={data.user.id} className="label-class label-text">
+                  {data.time} seconds / {data.mistakes} mistakes
+                </span>
+              ))}
+            </div>
+          </div>
+        </Box>
+      </div>
+      <div>
+        <Button
+          title="filled-btn"
+          text="BACK"
+          customStyle="my-button-left"
+          onClick={() => {
+            socket.emit("leaveRoom");
+            navigate(`/`);
+          }}
+        ></Button>
+        <Button
+          title="filled-btn"
+          text="PLAY AGAIN"
+          customStyle="my-button-right"
+          onClick={() => {
+            navigate(`/room/${roomCode}`, {
+              state: { nickname: nickname },
+            });
+          }}
+        ></Button>
+      </div>
+    </div>
+  );
 };
 
 export default Results;
